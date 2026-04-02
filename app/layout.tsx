fd4a3a61747a58a21e28c83678b3ddf33e2993c4
Thomas Gouges <thomasgouges@MacBook-Pro-de-Thomas.local>
2026-04-02 17:23:41 +0200

    Ajout Google Tag Manager (GTM-PG4GS2MS)

--- app/layout.tsx
+++ app/layout.tsx
@@ -55,7 +55,11 @@ export default function RootLayout({
 }) {
   return (
     <html lang="fr" className={geist.className}>
+      <head>
+        <script dangerouslySetInnerHTML={{ __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src='https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);})(window,document,'script','dataLayer','GTM-PG4GS2MS');` }} />
+      </head>
       <body className="bg-white text-gray-900 antialiased">
+        <noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-PG4GS2MS" height="0" width="0" style={{ display: "none", visibility: "hidden" }}></iframe></noscript>
         <CalInit />
         {children}
       </body>
