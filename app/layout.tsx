fdab48e8210c861d2a468819f6331832914a1f8c
Thomas Gouges <thomasgouges@MacBook-Pro-de-Thomas.local>
2026-04-02 17:30:55 +0200

    Fix Google Analytics et GTM avec next/script

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import { Geist } from "next/font/google";
+import Script from "next/script";
 import "./globals.css";
 import CalInit from "@/components/CalInit";
 
@@ -55,13 +56,10 @@ export default function RootLayout({
 }) {
   return (
     <html lang="fr" className={geist.className}>
-      <head>
-        <script async src="https://www.googletagmanager.com/gtag/js?id=G-FM63JGNETP" />
-        <script dangerouslySetInnerHTML={{ __html: `window.dataLayer=window.dataLayer||[];function gtag(){dataLayer.push(arguments);}gtag('js',new Date());gtag('config','G-FM63JGNETP');` }} />
-        <script dangerouslySetInnerHTML={{ __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src='https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);})(window,document,'script','dataLayer','GTM-PG4GS2MS');` }} />
-      </head>
       <body className="bg-white text-gray-900 antialiased">
-        <noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-PG4GS2MS" height="0" width="0" style={{ display: "none", visibility: "hidden" }}></iframe></noscript>
+        <Script src="https://www.googletagmanager.com/gtag/js?id=G-FM63JGNETP" strategy="afterInteractive" />
+        <Script id="ga-init" strategy="afterInteractive" dangerouslySetInnerHTML={{ __html: `window.dataLayer=window.dataLayer||[];function gtag(){dataLayer.push(arguments);}gtag('js',new Date());gtag('config','G-FM63JGNETP');` }} />
+        <Script id="gtm-init" strategy="afterInteractive" dangerouslySetInnerHTML={{ __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src='https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);})(window,document,'script','dataLayer','GTM-PG4GS2MS');` }} />
         <CalInit />
         {children}
       </body>
